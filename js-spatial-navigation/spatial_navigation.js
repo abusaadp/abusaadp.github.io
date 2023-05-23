5b1357eda180d650813e8f91c414cc8c5d60b3cd
Abu Saad Papa <papa.abu@gmail.com>
2023-05-23 14:32:01 +0300

    Update spatial_navigation.js

--- js-spatial-navigation/spatial_navigation.js
+++ js-spatial-navigation/spatial_navigation.js
@@ -483,6 +483,7 @@
 
     function getCurrentFocusedElement() {
       var activeElement = document.querySelector('[style="background: yellow; border: thick solid rgb(0, 0, 255);"]');
+      console.log("activeElement is ", activeElement);
       if (activeElement && activeElement !== document.body) {
           return activeElement;
       }
